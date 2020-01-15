de687ee29dc8b7ebdb1722190b06807622ce0290
psktcsharp <safeerlibya@gmail.com>
2020-01-15 14:32:18 +0100

    adding routes

--- client/pos-web-client/src/app/app.module.ts
+++ client/pos-web-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AuthComponent } from './auth/auth.component';
     AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
