f0f35ce510b0a991ead47d412237862ea4fb9487
psktcsharp <safeerlibya@gmail.com>
2020-01-16 11:48:15 +0100

    working on routing

--- client/pos-web-client/src/app/app.component.ts
+++ client/pos-web-client/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pos-web-client';
+  LoadedPath = 'sell'
+  onNavigate(selectedPath: string) {
+    this.LoadedPath = selectedPath;
+  }
 }
